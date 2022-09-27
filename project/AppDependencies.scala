983dbdbcb5de39b583df774fd39eede3f4d62e9e
Alex Rimmer <31283208+AlexRimmerHMRC@users.noreply.github.com>
2022-09-27 09:14:26 +0100

    ITSASU-1635 - Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "5.24.0"
+  val bootstrapPlayVersion = "7.4.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.14.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.28.0-play-28",
     "org.webjars" % "jquery" % "3.6.0"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapPlayVersion % "test, it",
     "org.jsoup" % "jsoup" % "1.13.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
