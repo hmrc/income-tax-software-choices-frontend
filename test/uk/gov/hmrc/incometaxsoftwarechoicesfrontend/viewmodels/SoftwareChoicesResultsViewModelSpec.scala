7ad7df02b941b84607fb580364a5a2791c70b97c
Ian Ralphs <29889314+ian-ralphs@users.noreply.github.com>
2025-07-15 21:01:21 +0100

    added tests for tabbed view

---
+++ test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/viewmodels/SoftwareChoicesResultsViewModelSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxsoftwarechoicesfrontend.viewmodels
+
+import org.scalatestplus.play.PlaySpec
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.models.VendorFilter.{Agent, Individual}
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.models.VendorFilterGroups._
+
+class SoftwareChoicesResultsViewModelSpec extends PlaySpec {
+
+  "SoftwareChoicesResultsViewModel" should {
+    "create an empty default model" which {
+      val model = SoftwareChoicesResultsViewModel()
+
+      "has empty all in one vendors" in {
+        model.allInOneVendors.vendors mustBe Seq.empty
+      }
+
+      "has empty other vendors list" in {
+        model.otherVendors.vendors mustBe Seq.empty
+      }
+
+      "has zeroResult set false" in {
+        model.zeroResults mustBe false
+      }
+
+      "has agent set false" in {
+        model.isAgent mustBe false
+      }
+    }
+  }
+
+}
