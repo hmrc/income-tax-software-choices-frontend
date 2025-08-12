508b2732f8e3c86db656875773c601319dd2b024
paddynski-opencast <128393110+paddynski-opencast@users.noreply.github.com>
2025-08-12 14:11:02 +0100

    [ITSASU-4231] Session timeout popup

---
+++ app/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/controllers/KeepAliveController.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxsoftwarechoicesfrontend.controllers
+
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.repositories.UserFiltersRepository
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.views.html.SessionExpiredView
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class KeepAliveController @Inject()(
+  repo: UserFiltersRepository
+)(implicit val ec: ExecutionContext,
+ mcc: MessagesControllerComponents) extends BaseFrontendController(mcc) {
+
+  def show(url: String): Action[AnyContent] = Action.async { implicit request =>
+    val sessionId = request.session.get("sessionId").getOrElse("")
+    for (
+      _ <- repo.keepAlive(sessionId)
+    ) yield {
+      Redirect(url)
+    }
+  }
+}
