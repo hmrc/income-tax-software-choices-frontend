48ce8a6678135c5ef3b74e41ef38f2b72b6ec1bb
ILee15 <89247111+ILee15@users.noreply.github.com>
2025-10-29 16:56:26 +0000

    ISC-125 interstital page content update

--- test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/views/ChoosingSoftwareViewSpec.scala
+++ test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/views/ChoosingSoftwareViewSpec.scala
@@ -55,6 +55,7 @@ class ChoosingSoftwareViewSpec extends ViewSpec {
       document.mainContent.selectNth("p", 6).text() shouldBe ChoosingSoftwareContent.para6
       document.mainContent.selectNth("p", 7).text() shouldBe ChoosingSoftwareContent.para7
       document.mainContent.selectNth("p", 8).text() shouldBe ChoosingSoftwareContent.para8
+      document.mainContent.selectNth("p", 9).text() shouldBe ChoosingSoftwareContent.para9
     }
 
     "have the correct bullet points" in {
@@ -96,15 +97,16 @@ private object ChoosingSoftwareContent {
   val bullet1 = "ready now"
   val bullet2 = "in development"
   val para3 = "All the software products in your results will let you or your agent:"
-  val bullet3 = "create, store and correct digital records"
+  val bullet3 = "create, store and correct digital records of your self-employment and property income and expenses"
   val bullet4 = "send your quarterly updates to HMRC"
   val h3 = "Submitting your 2026 to 2027 tax return"
-  val para4 = "The features you will need to submit your tax return are still being developed by some software providers. They are shown as ‘in development’."
-  val para5 = "These are features that software providers have either built " +
+  val para4 = "This is everything you need to start using Making Tax Digital for Income Tax from 6 April 2026."
+  val para5 = "The features you will need to submit your tax return are still being developed by some software providers. They are shown as ‘in development’."
+  val para6 = "These are features that software providers have either built " +
     "or committed to build ready for when you need to submit your 2026 to 2027 tax return."
   val h2two = "Making sure you get the right product"
-  val para6 = "Some of the listed products may have free trials or free versions, but you’ll have to pay for most of them."
-  val para7 = "HMRC is not responsible for the availability of products or making sure that the product you chose meets your current and future needs."
-  val para8 = "We recommend that you visit software providers’ websites to do more research before choosing a product."
+  val para7 = "Some of the listed products may have free trials or free versions, but you’ll have to pay for others."
+  val para8 = "HMRC is not responsible for the availability of products or making sure that the product you chose meets your current and future needs."
+  val para9 = "We recommend that you visit software providers’ websites to do more research before choosing a product."
   val button = "View your results"
 }
