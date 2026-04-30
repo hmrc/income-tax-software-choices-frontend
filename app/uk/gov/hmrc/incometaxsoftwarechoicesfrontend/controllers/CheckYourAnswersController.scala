661165b8012226ae67e2873e157cb64d8aaf11e9
josenewmano <98953155+Josenewmano@users.noreply.github.com>
2026-04-30 11:29:01 +0100

    Update yield block & add ITs

--- app/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/controllers/CheckYourAnswersController.scala
+++ app/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/controllers/CheckYourAnswersController.scala
@@ -20,7 +20,7 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Request}
 import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.config.AppConfig
 import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.controllers.actions.{RequireUserDataRefiner, SessionIdentifierAction}
 import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.models.JourneyType.{Check, Find}
-import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.models.SoftwareType.{Recognised, Unrecognised}
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.models.SoftwareType.Recognised
 import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.services.{PageAnswersService, SoftwareChoicesService}
 import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.views.helpers.SummaryListBuilder
 import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.views.html.CheckYourAnswersView
@@ -54,34 +54,38 @@ class CheckYourAnswersController @Inject()(view: CheckYourAnswersView,
       vendorFilters <- pageAnswersService.saveFiltersFromAnswers(request.sessionId)
       vendors = softwareChoicesService.getVendorsWithIntent(vendorFilters)
     } yield {
-      val foundVendor = vendors.find(v => request.softwareName.contains(v.vendor.name))
+      //TODO - Remove print lines in yield when new controllers are in place
+      println(Console.GREEN + vendors.map(_.vendor.name) + Console.RESET)
+      println(Console.YELLOW + request.softwareName + Console.RESET)
+      val foundVendor = vendors.find(v => request.softwareName == Some(v.vendor.name))
       println(Console.RED + foundVendor + Console.RESET)
-      // If not compliant condition is correct, remove foundVendor.isDefined from below, and just keep guard clauses?
-      (vendors.isEmpty, request.journey, request.softwareType, foundVendor.isDefined) match {
-        case (true, _, _, _) =>
+      val isQuarterlyReady = foundVendor.flatMap(_.quarterlyReady)
+      println(Console.RED + isQuarterlyReady + Console.RESET)
+      val isEoyReady = foundVendor.flatMap(_.eoyReady)
+      println(Console.RED + isEoyReady + Console.RESET)
+      (vendors.isEmpty, request.journey, request.softwareType, isQuarterlyReady, isEoyReady) match {
+        case (true, _, _, _, _) =>
           println(Console.BLUE + "zero results" + Console.RESET)
           Redirect(routes.ZeroSoftwareResultsController.show())
-        case (false, None, _, _) =>
+        case (false, None, _, _, _) =>
           println(Console.BLUE + "no journey, choosing software" + Console.RESET)
           Redirect(routes.ChoosingSoftwareController.show())
-        case (false, Some(Find), _, _)  =>
+        case (false, Some(Find), _, _, _)  =>
           println(Console.BLUE + "find journey, choosing software" + Console.RESET)
           Redirect(routes.ChoosingSoftwareController.show())
-        case (false, Some(Check), Some(Unrecognised), _)  =>
-          // Or the following??
-          // case (false, Some(Check), _, false)  =>
-          println(Console.BLUE + "check journey, not compliant" + Console.RESET)
-          Redirect(routes.CheckYourAnswersController.show())
-        case (false, Some(Check), Some(Recognised), true) if foundVendor.get.eoyReady.contains(true) =>
+        case (false, Some(Check), Some(Recognised), Some(true), Some(true)) =>
           println(Console.BLUE + "check journey, fully compliant" + Console.RESET)
           Redirect(routes.CheckYourAnswersController.show())
-        case (false, Some(Check), Some(Recognised), true) if foundVendor.get.eoyReady.contains(false) =>
+        case (false, Some(Check), Some(Recognised), Some(true), Some(false)) =>
           println(Console.BLUE + "check journey, partially compliant" + Console.RESET)
           Redirect(routes.CheckYourAnswersController.show())
-        case (false, Some(Check), Some(Recognised), true) if foundVendor.get.eoyReady.isEmpty =>
+        case (false, Some(Check), Some(Recognised), Some(true), None) =>
           println(Console.BLUE + "check journey, quarterly only" + Console.RESET)
           Redirect(routes.CheckYourAnswersController.show())
-        case (false, Some(Check), _, _) =>
+        case (false, Some(Check), Some(Recognised), None, None) =>
+          println(Console.BLUE + "check journey, not compliant" + Console.RESET)
+          Redirect(routes.CheckYourAnswersController.show())
+        case (false, Some(Check), _, _, _) =>
           println(Console.BLUE + "check journey, choosing software" + Console.RESET)
           Redirect(routes.ChoosingSoftwareController.show())
       }
