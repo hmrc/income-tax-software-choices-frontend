061086c249540d51a3764631d0d02b2804db604e
ian-ralphs <29889314+ian-ralphs@users.noreply.github.com>
2026-06-30 17:31:52 +0100

    added scripts.js for converting history POST calls to GETs

---
+++ app/assets/javascript/scripts.js
@@ -0,0 +1,4 @@
+// ensure POSTed pages are replaced with GET to avoid form resubmission errors
+if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
+    window.history.replaceState(null, null, window.location.href);
+}
