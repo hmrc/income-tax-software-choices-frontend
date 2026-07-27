43ee7263960e3417495aa8acf28187963299fd08
navithamuniraju-cpu <261942424+navithamuniraju-cpu@users.noreply.github.com>
2026-07-27 13:31:26 +0100

    ISC-793: Updated the spec files for para2

--- test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/views/FullyCompatibleViewSpec.scala
+++ test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/views/FullyCompatibleViewSpec.scala
@@ -76,7 +76,7 @@ private object FullyCompatibleContent {
   val heading2 = "What you should do next"
   val title = s"$heading1 - ${PageContentBase.title} - GOV.UK"
   val para1 = "Your software currently supports all the features you need for Making Tax Digital for Income Tax."
-  val para2 = "Learn more about your software and its upcoming features."
+  val para2 = "Learn more about your software and its upcoming features"
   val para3 = "If you want to use this software, you need to:"
   val bullet1Text = "sign up for Making Tax Digital for Income Tax (opens in new tab)"
   val bullet1Link = "https://www.gov.uk/guidance/sign-up-for-making-tax-digital-for-income-tax"
