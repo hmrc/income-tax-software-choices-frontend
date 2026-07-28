80afbcab45a518f45bb08effa9a70edc2aad6411
navithamuniraju-cpu <261942424+navithamuniraju-cpu@users.noreply.github.com>
2026-07-28 09:26:25 +0100

    ISC-792: Updated the code after latest welsh content changes.

--- test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/views/ZeroSoftwareResultsViewSpec.scala
+++ test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/views/ZeroSoftwareResultsViewSpec.scala
@@ -62,14 +62,6 @@ class ZeroSoftwareResultsViewSpec extends ViewSpec {
         document().mainContent.select("p").get(3).text mustBe ZeroSoftwareResultsViewContent.paragraph4
       }
 
-      "have a bullet list" which {
-        lazy val bulletList = document().mainContent.selectHead("ul.govuk-list.govuk-list--bullet")
-
-        "has a first point" in {
-          bulletList.selectNth("li", 1).text mustBe ZeroSoftwareResultsViewContent.bulletPoint1
-        }
-      }
-
        "display the exit survey link" in {
           val link = document().mainContent.select(".govuk-link").get(0)
           link.text shouldBe ZeroSoftwareResultsViewContent.exitSurveyLinkTitle
@@ -89,8 +81,7 @@ private object ZeroSoftwareResultsViewContent {
   val heading2 = "More software will be available soon"
   val paragraph2 = "Several all-in-one products are currently being developed."
   val paragraph3 = "We update this tool regularly to show what’s available. Please check back later for updates, which will include new software and changes to the features of existing software."
-  val paragraph4 = "You can also:"
-  val bulletPoint1 = "ask your agent or accountant about software, if you have one"
+  val paragraph4 = "If you have an agent or accountant, you can also ask them about software."
   val finish = "Finish"
   val exitSurveyLinkTitle = "Give feedback on this service (opens in new tab)"
   val exitSurveyLink = "http://localhost:9514/feedback/SOFTWAREMTDIT?useServiceNavigation"
