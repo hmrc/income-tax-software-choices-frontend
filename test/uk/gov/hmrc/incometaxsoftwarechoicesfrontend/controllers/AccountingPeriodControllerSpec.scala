54022cff23fcab7565d4f1b6452b6bb0b427d01f
josenewmano <98953155+Josenewmano@users.noreply.github.com>
2026-08-19 11:20:25 +0100

    Add AccountingPeriodControllerSpec to increase statement coverage

---
+++ test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/controllers/AccountingPeriodControllerSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxsoftwarechoicesfrontend.controllers
+
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import play.api.libs.json.Writes
+import play.api.mvc.Result
+import play.api.test.Helpers.{await, defaultAwaitTimeout}
+import uk.gov.hmrc.http.InternalServerException
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.controllers.actions.mocks.{MockRequireUserDataRefiner, MockSessionIdentifierAction}
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.forms.AccountingPeriodForm
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.models.*
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.models.AccountingPeriod.SixthAprilToFifthApril
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.queries.Settable
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.services.PageAnswersService
+import uk.gov.hmrc.incometaxsoftwarechoicesfrontend.views.html.AccountingPeriodView
+
+import scala.concurrent.Future
+
+class AccountingPeriodControllerSpec extends ControllerBaseSpec with MockSessionIdentifierAction with MockRequireUserDataRefiner {
+
+  "submit" when {
+    "throw an exception" when {
+
+      "user failed to save answers to Filters" in new Setup {
+        when(mockPageAnswersService.setPageAnswers(
+          any[UserFilters],
+          any[Settable[AccountingPeriod]],
+          any[AccountingPeriod]
+        )(any[Writes[AccountingPeriod]]))
+          .thenReturn(Future.successful(false))
+
+        val result: Future[Result] = controller().submit(true)(fakeRequest.post(AccountingPeriodForm.accountingPeriodForm, Seq(SixthAprilToFifthApril)))
+
+        intercept[InternalServerException](await(result)).message shouldBe "[AccountingPeriodController][submit] - Could not save accounting period"
+      }
+    }
+  }
+
+  class Setup {
+    val mockPageAnswersService: PageAnswersService = mock[PageAnswersService]
+    lazy val mockUserTypeView: AccountingPeriodView = mock[AccountingPeriodView]
+
+    def controller(): AccountingPeriodController = new AccountingPeriodController(
+      mockUserTypeView,
+      mockPageAnswersService,
+      fakeSessionIdentifierAction,
+      fakeRequireUserDataRefiner()
+    )(ec, mcc)
+  }
+
+}
