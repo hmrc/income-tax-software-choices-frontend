ad0a8caaa3360306a13438a68f7f305e586ca6a7
navithamuniraju-cpu <261942424+navithamuniraju-cpu@users.noreply.github.com>
2026-09-17 15:56:41 +0100

    ISC-930: Modified the file to follow the SearchSoftwareView file implementation

--- test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/views/ProductDetailsViewSpec.scala
+++ test/uk/gov/hmrc/incometaxsoftwarechoicesfrontend/views/ProductDetailsViewSpec.scala
@@ -628,7 +628,7 @@ class ProductDetailsViewSpec extends ViewSpec with BeforeAndAfterEach {
       }
 
       "has the getting started text" in {
-        document.select(".app-grey-box > p").text shouldBe ProductDetailsPage.gettingStartedText
+        document.select(".app-getting-started-box > p").text shouldBe ProductDetailsPage.gettingStartedText
       }
 
       "has a link to sign up for MTD" in {
